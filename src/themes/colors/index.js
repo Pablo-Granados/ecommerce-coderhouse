e41b9074e5bc9fd5a4eeda60f48f390762cb1a04
Pablo-Granados <pablogranados22@hotmail.com>
2023-07-13 00:17:49 -0300

    sumando switch y mas comonponentes

--- src/themes/colors/index.js
+++ src/themes/colors/index.js
@@ -1,9 +1,8 @@
 export const COLORS = {
-    primary: "#1D20B7",
-    secondary: "#B4C5E4",
-    text: "#FBFFF1",
-    background: "#37333C",
-    black: "#212529,",
-    white: "whitesmoke"
-
-}
+    primary: '#1D20B7', //azul fuerte
+    secondary: '#B4C5E4', // azul clarito
+    text: '#FDFFF7', //simil blanco
+    background: '#37333C', //negro
+    black: '#000000',
+    white: '#FFFFFF',
+  };
